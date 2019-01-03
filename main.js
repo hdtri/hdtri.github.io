dfa1e7dc4008ae395d7dfe8a23a13ce4480ced12
hdtri <hdtri@users.noreply.github.com>
2019-01-04 00:22:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,94 +1,14 @@
-const socket = io('https://serwebrtctrihd.herokuapp.com/');
-$('#div-chat').hide();
-let customConfig;
-$.ajax({
-  url: "https://service.xirsys.com/ice",
-  data: {
-    ident: "trihd",
-    secret: "aaa34290-c584-11e8-adb8-85894fb2751c",
-    domain: "global.xirsys.net/_turn/MyFirstApp",
-    application: "default",
-    room: "default",
-    secure: 1
-  },
-  success: function (data, status) {
-    // data.d is where the iceServers object lives
-    customConfig = data.d;
-    console.log(customConfig);
-  },
-  async: false
-});
-socket.on('DANH_SACH_ONLINE', arrUserInfo => {
-	$('#div-chat').show();
-	$('#div-dang-ky').hide();
-	arrUserInfo.forEach(user => {
-		const { ten, peerId } = user;
-		$('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-	});
-	socket.on('CO_NGUOI_DUNG_MOI', user => {
-		const { ten, peerId } = user;
-		$('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-	});
-	socket.on('AI_DO_NGAT_KET_NOI', peerId => {
-		$(`#${peerId}`).remove();
-	});
-});
-socket.on('DANG_KY_THAT_BAI', () => alert('Vui long chon username khac'));
-
-function openStream() {
-	const config ={ audio: true, video: true};
-	return navigator.mediaDevices.getUserMedia(config);
-}
-
-function playStream(idVideoTag, stream) {
-	const video = document.getElementById(idVideoTag);
-	video.srcObject = stream;
-	video.play();
-}
-//openStream ()
-//.then(stream => playStream('localStream', stream));
-const peer = new Peer({
-	key: 'peerjs',
-	host: 'webrtctrihd1407.herokuapp.com',
-	secure: true,
-	port: 443,
-	config: customConfig
-});
-
-peer.on('open', id => {
-	$('#my-peer').append(id);
-	$('#btnSignUp').click(() => {
-		const username = $('#txtUsername').val();
-		socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerId: id});
-	});
-});
-
-//caller
-$('#btnCall').click(() => {
-		const id = $('#remoteId').val();
-		openStream()
-		.then(stream => {
-			playStream('localStream', stream);
-			const call = peer.call(id, stream);
-			call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-		});
-});
-//Callee
-peer.on('call', call => {
-	openStream()
-	.then(stream => {
-		call.answer(stream);
-		playStream('localStream', stream);
-		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-	});
-});
-
-$('#ulUser').on('click', 'li', function() {
-	const id = $(this).attr('id');
-	openStream()
-		.then(stream => {
-			playStream('localStream', stream);
-			const call = peer.call(id, stream);
-			call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
-		});
-});
+var options = {
+      media: {
+        local: {
+          video: document.getElementById('localVideo')
+        },
+        remote: {
+          video: document.getElementById('remoteVideo'),
+          // This is necessary to do an audio/video call as opposed to just a video call
+          audio: document.getElementById('remoteVideo')
+        }
+      },
+      ua: {}
+    };
+var simple = new SIP.Web.Simple(options);
