5777fb93d6de4f2e131a9c0c83f59dd5f23c1da3
hdtri <hdtri@users.noreply.github.com>
2019-01-04 11:38:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 // On this codelab, you will be streaming only video (video: true).
 const mediaStreamConstraints = {
-  video: true,
+  video: false,
+  audio: true,
 };
 
 // Video element where stream will be placed.
