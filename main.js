e1697f5e7ab3249b71f5bc390153832ac41e85bf
hdtri <hdtri@users.noreply.github.com>
2019-01-04 11:26:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-/*var options = {
-      media: {
-        local: {
-          video: document.getElementById('localVideo')
-        },
-        remote: {
-          video: document.getElementById('remoteVideo'),
-          // This is necessary to do an audio/video call as opposed to just a video call
-          audio: document.getElementById('remoteVideo')
-        }
-      },
-      ua: {}
-    };
-*/    
-// var simple = new SIP.Web.Simple(options);
-function openStream() {
-	const config ={ audio: false, video: true};
-	return navigator.mediaDevices.getUserMedia(config);
+'use strict';
+
+// On this codelab, you will be streaming only video (video: true).
+const mediaStreamConstraints = {
+  video: true,
+};
+
+// Video element where stream will be placed.
+const localVideo = document.querySelector('video');
+
+// Local stream that will be reproduced on the video.
+let localStream;
+
+// Handles success by adding the MediaStream to the video element.
+function gotLocalMediaStream(mediaStream) {
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream;
 }
 
-function playStream(idVideoTag, stream) {
-	const video = document.getElementById(idVideoTag);
-	video.srcObject = stream;
-	video.play();
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error);
 }
+
+// Initializes media stream.
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
