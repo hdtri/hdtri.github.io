f254afc79fc17eff46cb9402f4370245a2802cb0
hdtri <hdtri@users.noreply.github.com>
2019-10-10 16:44:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@
             autostop: false,
             uri: uri_link,
             registerOptions: {
-                expires: 60
+                expires: 600
             },
             password: passwd,
             authorizationUser: username
